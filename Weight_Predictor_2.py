e5ecbcad028654190307d027eda6bb4737a52d34
Nishant Garg <nishantgarg76342@gmail.com>
2019-12-04 19:34:29 +0530

    *(Weight_Predictor_2) Fit model in PolynomialFeatures

--- Weight_Predictor_2.py
+++ Weight_Predictor_2.py
@@ -12,10 +12,8 @@ import pandas as pd
 
 dataset = pd.read_csv("Dataset_Fish.csv")
 
-
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
 
 one_hot_encoder = ColumnTransformer(transformers = [("encoder",OneHotEncoder(), ["Species"])], remainder = "passthrough")
 dataset = one_hot_encoder.fit_transform(dataset)
@@ -31,11 +29,39 @@ Y = dataset.iloc[:,7].values
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.4, random_state = 18)
 
 """ Scaling the Features Length1, Length2, Length3, Width, Height"""
 from sklearn.preprocessing import StandardScaler
 scaler = ColumnTransformer(transformers = [("scaler",StandardScaler(),[7,8,9,10,11])], remainder = "passthrough")
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+
+
+""" Using Polynomial Regression to create Complex Features"""
+from sklearn.preprocessing import PolynomialFeatures
+poly_fet = PolynomialFeatures(degree = 2)
+X_train_poly = poly_fet.fit_transform(X_train)
+X_test_poly = poly_fet.transform(X_test)
+
+""" Fitting the Complex Features in Linear Regression"""
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(X_train_poly,Y_train)
+y_pred_poly = reg.predict(X_test_poly)
+
+""" Getting the R^2 for the Model"""
+a_2 = reg.score(X_test_poly,Y_test)
+
+""" Getting the Error for each Prediction"""
+prediction_error = Y_test - y_pred_poly
+
+""" Finding the Mean Error in predictions"""
+dd = pd.DataFrame(prediction_error)
+dd.describe()
+
+
+
+""" LinearRegression vs sm.OLS model. Try both as sm.OLS has summary, R^2, Adj. R^2"""
+
 
